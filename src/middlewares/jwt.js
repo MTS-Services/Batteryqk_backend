c98a6653569ee43a040aea03ed7b16b0bf913a7b
Shariarhosain <shariarhosain131529@gmail.com>
2025-07-03 14:57:34 +0600

    jwt fix

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (uid) => {
   return jwt.sign({ uid }, process.env.SECRET_CODE, {
-    expiresIn: '1d', // Token expires in 1 day
+
+   
   });
 };
 
